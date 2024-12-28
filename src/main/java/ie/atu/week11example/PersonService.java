f04c45a31b114842dcfecf1abe547007cdf3f461
Aidan Malone <g00423306@atu.ie>
2024-12-28 13:44:31 +0000

    Got paymentClient working

--- src/main/java/ie/atu/week11example/PersonService.java
+++ src/main/java/ie/atu/week11example/PersonService.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
-    // Placeholder method to save a person (add to Db in the next two weeks)
+
+    public PersonService(PaymentClient paymentClient) {
+        this.paymentClient = paymentClient;
+    }
+
+    private PaymentClient paymentClient;
+
+
     public void savePerson(Person person) {
         System.out.println("Person saved: " + person);
+        String response = paymentClient.makePayment(person);
+        System.out.println(response);
     }
 
     // Placeholder method to retrieve a person by employeeId
